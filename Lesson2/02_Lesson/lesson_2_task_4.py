138ba5be3c3ba37f000a7520cf27920ab375db15
Culia7748 <culia7748@gmail.com>
2025-03-09 18:46:41 +0300

    Урок 2 с тренировками

--- Lesson2/02_Lesson/lesson_2_task_4.py
+++ Lesson2/02_Lesson/lesson_2_task_4.py
@@ -8,12 +8,12 @@ n = int(input("Введите число:"))
 
 def fizz_buzz(n):
     for x in range(1, n + 1):
-        if x % 3 == 0:
+        if x % 3 == 0 and x % 5 == 0:
+            print(f"FizzBuzz")
+        elif x % 3 == 0:
             print(f"Fizz")
         elif x % 5 == 0:
             print(f"Buzz")
-        elif x % 3 == 0 and x % 5 == 0:
-            print(f"FizzBuzz")
         else:
             print(x)
 
