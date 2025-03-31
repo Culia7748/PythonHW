8c33cc6b10201ce1994ae97d3ad3b1dbd331adcc
Culia7748 <culia7748@gmail.com>
2025-03-31 21:51:08 +0300

    Урок 3 с тренировками

--- Lesson5/05_lesson/lesson5_task4.py
+++ Lesson5/05_lesson/lesson5_task4.py
@@ -6,27 +6,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 # Открыть браузер FireFox
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+driver = webdriver.Firefox(service = FirefoxService(GeckoDriverManager().install()))
 
 # Перейти на страницу http://the-internet.herokuapp.com/login
 driver.get("http://the-internet.herokuapp.com/login")
 
 # В поле username ввести значение tomsmith
-username_input = driver.find_element (By.CSS_SELECTOR, "#username")
-username_input.send_keys("tomsmith", Keys.RETURN)
-sleep(3)
+username_input = driver.find_element(By.CSS_SELECTOR, "#username")
+username_input.send_keys("tomsmith")
+sleep(10)
 
 # В поле password ввести значение SuperSecretPassword!
-password_input = driver.find_element (By.CSS_SELECTOR, "#password")
-password_input.send_keys("SuperSecretPassword!", Keys.RETURN)
-sleep(3)
+password_input = driver.find_element(By.CSS_SELECTOR, "#password")
+password_input.send_keys("SuperSecretPassword!")
+sleep(10)
 
 # Нажать кнопку Login
 login_button = driver.find_element(By.CSS_SELECTOR, ".radius")
 login_button.click()
+sleep(5)
 
 # Вывести текст с зеленой плашки в консоль
-text = driver.find_element(By.CSS_SELECTOR, 'div.row  [alt="Fork me on GitHub"]').text
+text = driver.find_element(By.CSS_SELECTOR, 'div.flash.success').text
 print(text)
 
 # Закрыть браузер (метод quite())
