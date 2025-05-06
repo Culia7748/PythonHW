b4759136e3fe6769c05018e10880721288f1d2d4
Culia7748 <culia7748@gmail.com>
2025-05-06 22:24:12 +0300

    урок 8

--- 08_lesson/YouGileApi.py
+++ 08_lesson/YouGileApi.py
@@ -3,18 +3,8 @@ import requests
 class YouGileApi:
     def __init__(self, url):
         self.url = url
-        self.token = 'Cvp3faLCEVUqz+wnTckiAfe0f8xWOIg+KmLKw8y9GyAuc4VoMzM6DQOI-xCezfB+'
+        self.token = 'Подставить свой токен'
         self.project_id = 'da5b036a-139c-469e-aa9c-901f6423d25b'
-
-    def get_token(self, login='164ast@gmail.com', password='7cucBao-Io',
-                  companyId='36463eda-b086-4629-aee2-9004e171a820'):
-        creds = {
-            "login": login,
-            "password": password,
-            "companyId": companyId
-        }
-        resp = requests.post(self.url + '/api-v2/auth/keys', json=creds)
-        return resp.json()["key"]
 
     def new_project(self, title):
         my_headers = {}
@@ -25,9 +15,7 @@ class YouGileApi:
                 "da5b036a-139c-469e-aa9c-901f6423d25b": "admin"
             }
         }
-        print(my_headers["Authorization"])
         resp = requests.post(self.url + '/api-v2/projects', headers=my_headers, json=project)
-        print(resp.json()['id'])
         return resp
         # project_id = project.json()['id']
 
@@ -39,7 +27,6 @@ class YouGileApi:
         }
         project_add_point = self.url + '/api-v2/projects/'+ str(id)
         resp = requests.put(project_add_point, headers=my_headers, json=project)
-        print(project_add_point)
         return resp
 
     def get_project_id(self, project_id):
--- 08_lesson/test_yougile.py
+++ 08_lesson/test_yougile.py
@@ -14,17 +14,16 @@ def test_add_project_negative():
     title = ""
     project = api.new_project(title)
     assert project.status_code == 400
+    assert project.json()["message"][0] == "title should not be empty"
 
 @pytest.mark.positive
 def test_positive_change_project():
     title = "SkyPro1"
     project = api.new_project(title)
     project_id = project.json()['id']
-    print(project_id)
 
     new_title = "EasyCod"
     new_result = api.change_project(project_id, new_title)
-    print(new_result)
     assert new_result.status_code == 200
 
 @pytest.mark.negative
@@ -32,32 +31,25 @@ def test_negative_change_project():
     title = "SkyPro2"
     project = api.new_project(title)
     project_id = project.json()['id']
-    print(project_id)
 
     new_title = ""
     new_result = api.change_project(project_id, new_title)
-    print(new_result)
     assert new_result.status_code == 400
+    assert new_result.json()["message"][0] == "title should not be empty"
 
 @pytest.mark.positive
 def test_get_project_id_positive():
     title = "SkyPro3"
     project = api.new_project(title)
     project_id = project.json()['id']
-    print(project_id)
 
     get_project_id = api.get_project_id(project_id)
-    id_project = get_project_id.json()['id']
     title_project = get_project_id.json()['title']
-    print(id_project, title_project)
     assert get_project_id.status_code == 200
+    assert title_project == title
 
 @pytest.mark.negative
 def test_get_project_id_negative():
-    title = "SkyPro4"
-    project = api.new_project(title)
-    project_id = project.json()['id']
-    print(project_id)
-
     get_project_id = api.get_project_id('385730d5-303c-40c2-8e9e-3dea73f157c')
     assert get_project_id.status_code == 404
+    assert get_project_id.json()["message"] == "Проект не найден"
